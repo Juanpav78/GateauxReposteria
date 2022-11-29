3d78892b7ca5b567bf51a16bc8e1b3e7dcfd97fb
Juanpav78 <juanpav.78d@gmail.com>
2022-11-29 00:05:10 -0500

    hola

---
+++ app/root.jsx
@@ -0,0 +1,42 @@
+const {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} = require("@remix-run/react");
+
+export function links(){
+  return[{
+      rel: 'icon',
+      href: style,
+  }]
+}
+
+export const meta = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div className="class">
+        <h1>HOLA</h1>
+        </div>
+        
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
